9e4e44f6d5b9a924c788bf3d872bbe27fe0a5f64
Denis <denis.were@student.moringaschool.com>
2023-03-16 05:06:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph) {cats.push("Ralph");}
+function destructivelyPrependCat(Bob) {cats.unshift("Bob");}
+function destructivelyRemoveLastCat(Garfield) {cats.pop();}
+cats;
+function destructivelyRemoveFirstcat(Milo) {cats.shift();}
+cats;
+function appendCat(newCat) {const newCat=[cats.slice(0,2),"Broom",];}
