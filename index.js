2cfeb9a4c71a7778bfbcb1c66383b913d4287376
Denis <denis.were@student.moringaschool.com>
2023-03-20 18:05:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const cats = ["Milo", "Otis", "Garfield"];
-function destructivelyAppendCat(Ralph) {cats.push("Ralph");}
-function destructivelyPrependCat(Bob) {cats.unshift("Bob");}
-function destructivelyRemoveLastCat(Garfield) {cats.pop();}
-cats;
-function destructivelyRemoveFirstcat(Milo) {cats.shift();}
-cats;
-function appendCat(newCat) {const newCat=[cats.slice(0,2),"Broom",];}
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    return[...cats,"Broom"]
+}
+function PrependCat(Arnold){
+    return["Arnold", ...cats];
+}
+function removeLastCat(){
+    return cats.slice(0, -1);
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
